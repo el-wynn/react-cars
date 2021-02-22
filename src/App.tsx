084346d29879dfc22192cd9f6aa04d4a382002e7
Elwynn Neglokpe <elwynn.neglokpe@gmail.com>
2021-02-22 14:43:56 +0100

    Level 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
+import { ICars, getCars } from './cars'
 
-const App = () => (
+const App: React.FC = () => {
+
+  const data: ICars[]  = getCars();
+  if (data.length ==0) return null;
+  
+  return(
   <div>
-  <h1>Hello!</h1>
-  <p>
-    Edit this file and linked JS/CSS, changes will appear directly in your
-    browser.
-  </p>
-</div>
-);
+    <h1>Hello!</h1>
+    <div>
+      <ul className='cars'>
+        {data.map( (car) => (
+          <li key={car.id}>
+            <p>Brand : {car.brand}</p>
+            <p>Model : {car.model}</p>
+            <p>Price/day : {car.pricePerDay}</p>
+            <p>Price/km : {car.pricePerKm}</p>
+            <p>Brand : {car.brand}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+  );
+};
 
 export default App;
---
+++ src/cars.ts
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+export interface ICars {
+    id: number;
+    picturePath: string;
+    brand: string;
+    model: string;
+    pricePerDay: number;
+    pricePerKm: number;
+    availability: { maxDuration: number, maxDistance: number };
+}
+
+export const getCars = () => {
+    const [cars,setCars] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+    axios
+        .get<ICars[]>('./cars.json')
+        .then(response => {
+            setCars(response.data);
+            console.log(response.data[0])
+        })
+        .catch(ex => {
+            setError(ex.response);
+        })
+    }, []);
+
+    return cars;
+}
